e8dd573ac2b460848a6e9a8d7542251f9e25f143
Gabriel Lugo Méndez <gabriel.lugo.mendez@gmail.com>
2023-11-27 10:33:04 +0100

    styling header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,9 @@ import "../css/Header.css";
 function Header() {
   return (
     <header>
+      <div className="background-image">
+        <img src="https://picsum.photos/1920/1080?grayscale" alt="Background" />
+      </div>
       <h1>Dictionary</h1>
     </header>
   );
